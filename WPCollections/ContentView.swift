6f9d61d34ef8dedb77564e2868b9c8d1a875323f
Fabian Dean <fabian@fabiandean.dev>
2020-06-22 21:48:58 -0700

    Initial Commit

---
+++ WPCollections/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  WPCollections
+//
+//  Created by Fabian Flores on 6/22/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
